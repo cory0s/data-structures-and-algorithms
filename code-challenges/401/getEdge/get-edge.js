ce50875c4a73cc0d64b88c8865a52c81ac52b511
Cory Henderson <chenderson19899@gmail.com>
2019-05-07 15:14:49 -0700

    getEdge

--- code-challenges/401/getEdge/get-edge.js
+++ code-challenges/401/getEdge/get-edge.js
@@ -1,7 +1,25 @@
 'use strict';
 
-let Graph = require('../../../Data-Structures/graph/graph.js');
 
 function getEdge(graph, arr){
-    
+    let price = 0;
+
+    if(!graph.adjacencyList[arr[0]]){
+        return false;
+    }
+
+    for(let i=0; i < arr.length-1; i++){
+        graph.getNeighbors(arr[i]).forEach(neighbor => {
+            if(arr[i+1] === neighbor){
+                console.log(neighbor);
+                price = price + neighbor[arr[i]].weight;
+            } else {
+                return false;
+            }
+
+        })
+    }
+    return price;
 }
+
+module.exports = getEdge;
